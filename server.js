7691e59d7f75aad0b3ba3377e694d6b86f18655c
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2018-01-20 16:15:55 +0200

    express

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const express= require('express')
+const http = require('http')
+const url = require('url')
 const WebSocket = require('ws')
 
-const wss = new WebSocket.Server({ port: process.env.PORT || 8080 })
+const app = express()
+const server = http.createServer(app)
+const wss = new WebSocket.Server({ server })
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(express.static('.'))
 
 wss.broadcast = data => wss.clients.forEach(client => {
     if (client.readyState === WebSocket.OPEN) {
         client.send(data)
     }
+})
+
+app.post('/broadcast', (req, res) => {
+    wss.broadcast(JSON.stringify(req.body))
+    res.sendStatus(200)
 })
 
 wss.on('connection', function connection(ws) {
@@ -15,4 +32,6 @@ wss.on('connection', function connection(ws) {
     ws.on('error', error => console.log('error', ws.client, error.message))
 
     ws.on('message', wss.broadcast)
-});
+})
+
+server.listen(process.env.PORT || 8080)
